3d29178585bedbcc57a91fc218bdce8d0bb390c2
DanielZanad <daniel.zanad.dz@gmail.com>
2022-06-18 16:23:27 -0300

    feat: implementing insert_pet in PetRepository

--- src/infra/entities/pets.py
+++ src/infra/entities/pets.py
@@ -6,9 +6,9 @@ from src.infra.config import Base
 class AnimalTypes(enum.Enum):
     """Defining Animals Types"""
 
-    dog = ("dog",)
-    cat = ("cat",)
-    fish = ("fish",)
+    dog = "dog"
+    cat = "cat"
+    fish = "fish"
     turtle = "turtle"
 
 
