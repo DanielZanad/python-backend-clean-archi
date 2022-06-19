7762d840c973bb18c754d3a16f97676b14be9221
DanielZanad <daniel.zanad.dz@gmail.com>
2022-06-19 12:46:26 -0300

    feat: implementing select_pet in PetRepository

--- src/infra/repo/pet_repository_test.py
+++ src/infra/repo/pet_repository_test.py
@@ -1,6 +1,9 @@
 from faker import Faker
-from .pet_repository import PetRepository
+from src.infra.entities import Pets
 from src.infra.config import DBConnectionHandler
+from src.infra.entities.pets import AnimalTypes
+from .pet_repository import PetRepository
+
 
 faker = Faker()
 pet_repository = PetRepository()
@@ -27,3 +30,38 @@ def test_insert_pet():
     assert new_pet.specie == query_user.specie
     assert new_pet.age == query_user.age
     assert new_pet.user_id == query_user.user_id
+
+    engine.execute("DELETE FROM pets WHERE id='{}';".format(new_pet.id))
+
+
+def test_select_pet():
+    """Should select a pet in Pets table and comapre it"""
+    pet_id = faker.random_number(digits=4)
+    name = faker.name()
+    specie = "fish"
+    age = faker.random_number(digits=1)
+    user_id = faker.random_number()
+
+    specie_mock = AnimalTypes("fish")
+    data = Pets(id=pet_id, name=name, specie=specie_mock, age=age, user_id=user_id)
+
+    # SQL Commands
+
+    engine = db_connection_handler.get_engine()
+    engine.execute(
+        "INSERT INTO pets (id, name, specie, age, user_id) VALUES ('{}','{}', '{}', '{}', '{}');".format(
+            pet_id, name, specie, age, user_id
+        )
+    )
+
+    query_pets1 = pet_repository.select_pet(pet_id=pet_id)
+
+    query_pets2 = pet_repository.select_pet(user_id=user_id)
+
+    query_pets3 = pet_repository.select_pet(pet_id=pet_id, user_id=user_id)
+
+    assert data in query_pets1
+    assert data in query_pets2
+    assert data in query_pets3
+
+    engine.execute("DELETE FROM pets WHERE id='{}';".format(pet_id))
