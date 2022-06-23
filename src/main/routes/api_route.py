3d3995db9a399430593d68b05d5429ea6c6c9e60
DanielZanad <daniel.zanad.dz@gmail.com>
2022-06-23 11:16:21 -0300

    feat: Implementing register_user Route with flask

--- src/main/routes/api_route.py
+++ src/main/routes/api_route.py
@@ -1,12 +1,31 @@
-from flask import Blueprint, jsonify
-
-# from src.main.composer import register_user_composer
+from flask import Blueprint, jsonify, request
+from src.main.adapter import flask_adapter
+from src.main.composer import register_user_composer
 
 api_routes_bp = Blueprint("api_routes", __name__)
 
 
-@api_routes_bp.route("/api", methods=["GET"])
-def something():
-    """teste"""
+@api_routes_bp.route("/api/users", methods=["POST"])
+def register_user():
+    """Register User route"""
 
-    return jsonify({"Programador": "Lhama"})
+    message = {}
+    response = flask_adapter(request=request, api_route=register_user_composer())
+
+    if response.status_code < 300:
+
+        message = {
+            "Type": "users",
+            "id": response.body.id,
+            "attributes": {"name": response.body.name},
+        }
+
+        return jsonify({"data": message}), response.status_code
+
+    # Handling errors
+    return (
+        jsonify(
+            {"error": {"status": response.status_code, "title": response.body["error"]}}
+        ),
+        response.status_code,
+    )
